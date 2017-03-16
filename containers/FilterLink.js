4acb8c3c7edb0f4825be8aefaa8745761125d6ff
Ada Dyś <ada.dys@espeo.eu>
2017-03-16 16:22:27 +0100

    Create component containers

---
+++ containers/FilterLink.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../actions'
+import Link from '../components/Link'
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    }
+}
+
+const mapDispatchToProps = (dispach, ownProps) => {
+    return {
+        onClick: () => {
+            dispach(setVisibilityFilter(ownProps.filter))
+        }
+    }
+}
+
+const FilterLink = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Link)
+
+export default FilterLink
